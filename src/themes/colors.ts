8aea8672d4317019efb419b52764e36d0122c8eb
huy.phamnhat <huy.phamnhat@asnet.com.vn>
2025-01-07 14:55:07 +0700

    feat: define color themes

---
+++ src/themes/colors.ts
@@ -0,0 +1,34 @@
+export const colors = {
+  primary: '#242424',
+  secondary: '#303030',
+  tertiary: '#606060',
+  quaternary: '#808080',
+  white: '#ffffff',
+  disabled: '#999999',
+
+  divider: '#bdbdbd',
+
+  text: {
+    primary: '#242424',
+    secondary: '#303030',
+    tertiary: '#606060',
+    quaternary: '#808080',
+    alternative: '#909090',
+  },
+
+  border: {
+    primary: '#e0e0e0',
+    secondary: '#f0f0f0',
+  },
+
+  background: {
+    primary: '#242424',
+    secondary: '#f0f0f0',
+    tertiary: '#60606066',
+  },
+
+  shadow: {
+    primary: '#8A959E33',
+    secondary: '#30303040',
+  },
+};
