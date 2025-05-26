0dca7fee5dd97317d7e0b6d56f9f4714314bb0ca
huy.phamnhat <huy.phamnhat@asnet.com.vn>
2025-05-26 14:33:40 +0700

    refactor: improve HomeScreen params type

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,146 +1,151 @@
 import {memo, useCallback, useRef, useState} from 'react';
 import {ActivityIndicator, Animated, StyleSheet, View} from 'react-native';
 import {CategoryList, ProductList} from 'src/components';
+
 // Components
 import {Button, Text, TextInput} from 'src/components/common';
+
 // Icons
 import {CartIcon, SearchIcon} from 'src/components/icons';
+
 // Constants
 import {CATEGORIES} from 'src/constants';
+
 // Hooks
 import {useDebounce, useGetInfinitiveProducts} from 'src/hooks';
+
 // Types & Interfaces
-import {StackNavigation} from 'src/interfaces';
+import {HomeTabScreenProps} from 'src/interfaces/navigation';
+
 // Themes
 import {colors} from 'src/themes';
 
-export interface HomeScreenProps {
-  navigation: StackNavigation;
-}
-const HomeScreen = memo(({navigation: {navigate}}: HomeScreenProps) => {
-  const [category, setCategory] = useState<string>(CATEGORIES[0].title);
-  const {
-    value: searchQuery,
-    debouncedValue: debouncedSearchQuery,
-    setValue: setSearchQuery,
-  } = useDebounce('', 500);
-  const [isSearchVisible, setIsSearchVisible] = useState(false);
+const HomeScreen = memo(
+  ({navigation: {navigate}}: HomeTabScreenProps<'Home'>) => {
+    const [category, setCategory] = useState<string>(CATEGORIES[0].title);
+    const {
+      value: searchQuery,
+      debouncedValue: debouncedSearchQuery,
+      setValue: setSearchQuery,
+    } = useDebounce('', 500);
+    const [isSearchVisible, setIsSearchVisible] = useState(false);
 
-  const searchAnimation = useRef(new Animated.Value(0)).current;
+    const searchAnimation = useRef(new Animated.Value(0)).current;
 
-  const {
-    data = [],
-    isLoading,
-    fetchNextPage,
-    hasNextPage,
-    resetData,
-    isError,
-    error,
-  } = useGetInfinitiveProducts({
-    category,
-    name: debouncedSearchQuery,
-  });
+    const {
+      data = [],
+      isLoading,
+      fetchNextPage,
+      hasNextPage,
+      resetData,
+      isError,
+      error,
+    } = useGetInfinitiveProducts({
+      category,
+      name: debouncedSearchQuery,
+    });
 
-  const toggleSearch = useCallback(() => {
-    const toValue = isSearchVisible ? 0 : 1;
+    const toggleSearch = useCallback(() => {
+      const toValue = isSearchVisible ? 0 : 1;
 
-    Animated.timing(searchAnimation, {
-      toValue,
-      duration: 300,
-      useNativeDriver: false,
-    }).start();
+      Animated.timing(searchAnimation, {
+        toValue,
+        duration: 300,
+        useNativeDriver: false,
+      }).start();
 
-    setIsSearchVisible(!isSearchVisible);
+      setIsSearchVisible(!isSearchVisible);
 
-    if (isSearchVisible) {
-      setSearchQuery('');
-    }
-  }, [isSearchVisible, searchAnimation, setSearchQuery]);
+      if (isSearchVisible) {
+        setSearchQuery('');
+      }
+    }, [isSearchVisible, searchAnimation, setSearchQuery]);
 
-  const handleCartPress = useCallback(() => navigate('Cart'), [navigate]);
+    const handleCartPress = useCallback(() => navigate('Cart'), [navigate]);
 
-  const handleSetCategory = useCallback(
-    (categoryTitle: string) => {
-      categoryTitle !== category && setCategory(categoryTitle);
-    },
-    [category, setCategory],
-  );
+    const handleSetCategory = useCallback(
+      (categoryTitle: string) => {
+        categoryTitle !== category && setCategory(categoryTitle);
+      },
+      [category, setCategory],
+    );
 
-  const handleProductCardPress = useCallback(
-    (id: string) => () => {
-      navigate('ProductDetail', {
-        id,
-      });
-    },
-    [navigate],
-  );
-  const searchHeight = searchAnimation.interpolate({
-    inputRange: [0, 1],
-    outputRange: [0, 40],
-  });
+    const handleProductCardPress = useCallback(
+      (id: string) => () => {
+        navigate('ProductDetail', {
+          id,
+        });
+      },
+      [navigate],
+    );
+    const searchHeight = searchAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, 40],
+    });
 
-  const searchOpacity = searchAnimation.interpolate({
-    inputRange: [0, 1],
-    outputRange: [0, 1],
-  });
+    const searchOpacity = searchAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, 1],
+    });
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <SearchIcon onPress={toggleSearch} />
-        <View style={styles.headerTitle}>
-          <Text font="GelasioNormal" size="base" textVariant="alternative">
-            Make home
-          </Text>
-          <Text font="GelasioBold" size="base">
-            BEAUTIFUL
-          </Text>
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <SearchIcon onPress={toggleSearch} />
+          <View style={styles.headerTitle}>
+            <Text font="GelasioNormal" size="base" textVariant="alternative">
+              Make home
+            </Text>
+            <Text font="GelasioBold" size="base">
+              BEAUTIFUL
+            </Text>
+          </View>
+          <Button
+            style={styles.button}
+            bgVariant="none"
+            IconLeft={<CartIcon />}
+            onPress={handleCartPress}
+          />
         </View>
-        <Button
-          style={styles.button}
-          bgVariant="none"
-          IconLeft={<CartIcon />}
-          onPress={handleCartPress}
-        />
+
+        <Animated.View
+          style={[
+            styles.searchContainer,
+            {
+              height: searchHeight,
+              opacity: searchOpacity,
+            },
+          ]}>
+          <TextInput
+            inputSize="sm"
+            placeholder="Search products..."
+            value={searchQuery}
+            onChangeText={setSearchQuery}
+            autoCapitalize="none"
+          />
+        </Animated.View>
+
+        <CategoryList category={category} setCategory={handleSetCategory} />
+
+        {isLoading ? (
+          <View style={styles.wrapper}>
+            <ActivityIndicator size="large" color="black" />
+          </View>
+        ) : (
+          <ProductList
+            fetchNextPage={isError ? undefined : fetchNextPage}
+            hasNextPage={hasNextPage}
+            products={data}
+            isRefreshing={isLoading}
+            resetData={resetData}
+            handlePress={handleProductCardPress}
+            errorMessage={error?.message}
+          />
+        )}
       </View>
-
-      <Animated.View
-        style={[
-          styles.searchContainer,
-          {
-            height: searchHeight,
-            opacity: searchOpacity,
-          },
-        ]}>
-        <TextInput
-          inputSize="sm"
-          placeholder="Search products..."
-          value={searchQuery}
-          onChangeText={setSearchQuery}
-          autoCapitalize="none"
-        />
-      </Animated.View>
-
-      <CategoryList category={category} setCategory={handleSetCategory} />
-
-      {isLoading ? (
-        <View style={styles.wrapper}>
-          <ActivityIndicator size="large" color="black" />
-        </View>
-      ) : (
-        <ProductList
-          fetchNextPage={isError ? undefined : fetchNextPage}
-          hasNextPage={hasNextPage}
-          products={data}
-          isRefreshing={isLoading}
-          resetData={resetData}
-          handlePress={handleProductCardPress}
-          errorMessage={error?.message}
-        />
-      )}
-    </View>
-  );
-});
+    );
+  },
+);
 
 const styles = StyleSheet.create({
   container: {
