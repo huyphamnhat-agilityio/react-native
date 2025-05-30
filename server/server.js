daaf851252e3a6373aa441b033abd96c41ecf691
huy.phamnhat <huy.phamnhat@asnet.com.vn>
2025-05-30 14:53:23 +0700

    feat: apply json server auth

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const jsonServer = require("json-server");
+const auth = require("json-server-auth");
 
 const server = jsonServer.create();
-
 const router = jsonServer.router("db.json");
+
 const middlewares = jsonServer.defaults();
 
+const rules = auth.rewriter({
+  products: 440,
+});
+
+server.db = router.db;
+
 server.use(middlewares);
-server.use("/api", router);
+server.use(rules);
+server.use(auth);
+server.use(router);
+
 server.listen(process.env.PORT || 5000, () => {
   console.log("JSON Server is running");
 });
