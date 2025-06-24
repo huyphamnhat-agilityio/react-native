4578bb7e7dd8a9756ab47a2cd17b7e01a574aab5
huy.phamnhat <huy.phamnhat@asnet.com.vn>
2025-06-24 15:06:51 +0700

    feat: update mock data image

--- server/generateData.js
+++ server/generateData.js
@@ -12,7 +12,33 @@ function getRandomNumber(min, max) {
 
 function generateAdditionalProducts(startId, count) {
   const categories = ["chair", "table", "armchair", "bed", "lamp"];
-  const placeholderImage = "https://i.ibb.co/hJ1BGC1K/placeholder-image.jpg";
+  const placeholderImages = [
+    "https://m.media-amazon.com/images/I/711I3okfOpL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/61tKBisT-fL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/81TkJswJG0L._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/91rstt0EjJL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/61M0XLr7leL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/51vsLXXdogL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/71LWhgxJCaL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/91dOElUKYkL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/71rUHEn-sDL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/81wxsyggYnL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/71MddU5MM6L._AC_SL1024_.jpg",
+    "https://m.media-amazon.com/images/I/71FOaSg3vML._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/516Pw+oFelL._AC_.jpg",
+    "https://m.media-amazon.com/images/I/61XPAy+52+L._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/51SdBekg4vL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/81bSNmm-XUL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/71X8X1T44RL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/81JJfFGNgqL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/61SSdt8x+XL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/811sKBEFYxL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/910weRS40sL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/81B3z8MoqfL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/91h0xn+XfcL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/510aZ29xudL._AC_SL1500_.jpg",
+    "https://m.media-amazon.com/images/I/817l+VzWGWL._AC_SL1500_.jpg",
+  ];
 
   const products = [];
 
@@ -21,6 +47,8 @@ function generateAdditionalProducts(startId, count) {
     const name = `Mock product ${id}`;
     const description = `Mock description of product ${id}`;
     const category = categories[Math.floor(Math.random() * categories.length)];
+    const placeholderImage =
+      placeholderImages[Math.floor(Math.random() * placeholderImages.length)];
     const price = parseFloat(getRandomNumber(10, 500).toFixed(2));
     const stock = Math.floor(getRandomNumber(10, 1000));
     const rating = parseFloat(getRandomNumber(3.0, 5.0).toFixed(1));
@@ -49,7 +77,7 @@ function generateAdditionalProducts(startId, count) {
 
 const existingData = JSON.parse(fs.readFileSync("db.json", "utf-8"));
 
-const existingProducts = existingData.products || [];
+const existingProducts = existingData.products.slice(0, 25) || [];
 
 const additionalProducts = generateAdditionalProducts(26, 975);
 
